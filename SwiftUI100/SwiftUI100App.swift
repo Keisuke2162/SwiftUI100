6657e5ea4ef6878d153da086a4e2d1483517a637
Keisuke2162 <applabo.kei@gmail.com>
2023-08-01 03:53:18 +0900

    Initial Commit

---
+++ SwiftUI100/SwiftUI100App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI100App.swift
+//  SwiftUI100
+//
+//  Created by Kei on 2023/08/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
