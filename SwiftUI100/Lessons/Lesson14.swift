1a65381d74aaa95ecb4c53447eb88eac5ecfd22a
Keisuke2162 <applabo.kei@gmail.com>
2023-08-06 03:40:07 +0900

    Lesson14

---
+++ SwiftUI100/Lessons/Lesson14.swift
@@ -0,0 +1,34 @@
+//
+//  Lesson14.swift
+//  SwiftUI100
+//
+//  Created by Kei on 2023/08/06.
+//
+
+import SwiftUI
+
+struct Lesson14: View {
+    @State private var isShowAlert = false
+
+    var body: some View {
+        Button {
+            self.isShowAlert = true
+        } label: {
+            Image("kuma")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+        }.alert(isPresented: $isShowAlert) {
+            Alert(title: Text("アラート"),
+                  message: Text("Message"),
+                  primaryButton: .default(Text("A")),
+                  secondaryButton: .default(Text("B")))
+        }
+    }
+}
+
+struct Lesson14_Previews: PreviewProvider {
+    static var previews: some View {
+        Lesson14()
+    }
+}
