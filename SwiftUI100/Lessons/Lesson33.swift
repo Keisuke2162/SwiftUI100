924bcb4f53758b5a7e083c023e78812ef896ae95
Keisuke2162 <applabo.kei@gmail.com>
2023-08-19 14:20:20 +0900

    Lesson33

---
+++ SwiftUI100/Lessons/Lesson33.swift
@@ -0,0 +1,20 @@
+//
+//  Lesson33.swift
+//  SwiftUI100
+//
+//  Created by Kei on 2023/08/19.
+//
+
+import SwiftUI
+
+struct Lesson33: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Lesson33_Previews: PreviewProvider {
+    static var previews: some View {
+        Lesson33().previewLayout(.fixed(width: 1344, height: 621))
+    }
+}
