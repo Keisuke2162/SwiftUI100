a462f6b8a38d8f5b16fc9c3d1990f51686af3721
Keisuke2162 <applabo.kei@gmail.com>
2023-08-23 00:34:20 +0900

    Lesson47(Skip)

---
+++ SwiftUI100/Lessons/Lesson47.swift
@@ -0,0 +1,22 @@
+//
+//  Lesson47.swift
+//  SwiftUI100
+//
+//  Created by Kei on 2023/08/23.
+//
+
+import SwiftUI
+
+// MEMO: 内容がLesson46と同じっぽいのでスキップする
+
+struct Lesson47: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Lesson47_Previews: PreviewProvider {
+    static var previews: some View {
+        Lesson47()
+    }
+}
