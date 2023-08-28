caeabb4ee3ad46fbcba9cf9c93626f6ad0509ed4
Keisuke2162 <applabo.kei@gmail.com>
2023-08-29 02:55:35 +0900

    Lesson59(Skip)

---
+++ SwiftUI100/Lessons/Lesson59.swift
@@ -0,0 +1,22 @@
+//
+//  Lesson59.swift
+//  SwiftUI100
+//
+//  Created by Kei on 2023/08/29.
+//
+
+import SwiftUI
+
+// MEMO: SKIP
+
+struct Lesson59: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Lesson59_Previews: PreviewProvider {
+    static var previews: some View {
+        Lesson59()
+    }
+}
